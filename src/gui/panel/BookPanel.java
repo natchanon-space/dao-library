97711aad8ad47c7434e0c1a6709b21543de97f9f
natchanon-space <natchanon.space@gmail.com>
2022-04-24 16:26:29 +0700

    templete for BookPanel

--- src/gui/panel/BookPanel.java
+++ src/gui/panel/BookPanel.java
@@ -11,22 +11,83 @@ import java.sql.SQLException;
 
 public class BookPanel extends JPanel {
 
+    private BookDao dao;
+
     private JTable table;
     private JScrollPane scrollPane;
     private String[] columnNames = {"ID", "Title", "Author"};
-    private BookDao dao;
+
+    private JPanel addFormPanel;
+    private JPanel deleteFormPanel;
+    private JPanel updateFormPanel;
 
     public BookPanel() throws SQLException {
         dao = (BookDao) new DaoFactory().getDao(DaoFactory.DaoType.BOOK_DAO);
 
         initTable();
+        initAddForm();
+        initDeleteForm();
+        initUpdateForm();
 
-        setLayout(new BorderLayout());
-        add(scrollPane, BorderLayout.CENTER);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(addFormPanel);
+        add(deleteFormPanel);
+        add(updateFormPanel);
+        add(scrollPane);
+    }
+
+    private void initAddForm() {
+        addFormPanel = new JPanel();
+
+        JTextField titleField = new JTextField(20);
+        JTextField authorField = new JTextField(20);
+        JButton addButton = new JButton("add");
+
+        addFormPanel.add(new JLabel("[New Book]"));
+        addFormPanel.add(new JLabel("title: "));
+        addFormPanel.add(titleField);
+        addFormPanel.add(new JLabel("author: "));
+        addFormPanel.add(authorField);
+        addFormPanel.add(addButton);
+    }
+
+    private void initDeleteForm() {
+        deleteFormPanel = new JPanel();
+
+        JTextField idField = new JTextField(5);
+        JButton deleteButton = new JButton("delete");
+
+        deleteFormPanel.add(new JLabel("[Delete]"));
+        deleteFormPanel.add(new JLabel("id: "));
+        deleteFormPanel.add(idField);
+        deleteFormPanel.add(deleteButton);
+    }
+
+    private void initUpdateForm() {
+        updateFormPanel = new JPanel();
+
+        JTextField idField = new JTextField(5);
+        JTextField titleField = new JTextField(20);
+        JTextField authorField = new JTextField(20);
+        JButton updateButton = new JButton("Update");
+
+        updateFormPanel.add(new JLabel("[Update]"));
+        updateFormPanel.add(new JLabel("id: "));
+        updateFormPanel.add(idField);
+        updateFormPanel.add(new JLabel("title: "));
+        updateFormPanel.add(titleField);
+        updateFormPanel.add(new JLabel("author: "));
+        updateFormPanel.add(authorField);
+        updateFormPanel.add(updateButton);
     }
 
     private void initTable() throws SQLException {
-        DefaultTableModel dtm = new DefaultTableModel(0, 0);
+        DefaultTableModel dtm = new DefaultTableModel(0, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         dtm.setColumnIdentifiers(columnNames);
 
         for(Book b: dao.getAll()) {
