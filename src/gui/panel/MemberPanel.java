ee6ea7679d2685edf1bf25e884ea7f0e0b0365b6
natchanon-space <natchanon.space@gmail.com>
2022-04-24 16:38:05 +0700

    add template for MemberPanel

--- src/gui/panel/MemberPanel.java
+++ src/gui/panel/MemberPanel.java
@@ -18,17 +18,71 @@ public class MemberPanel extends JPanel {
     private String[] columnNames = {"ID", "Name"};
     private MemberDao dao;
 
+    private JPanel addFormPanel;
+    private JPanel deleteFormPanel;
+    private JPanel updateFormPanel;
+
     public MemberPanel() throws SQLException {
         dao = (MemberDao) new DaoFactory().getDao(DaoFactory.DaoType.MEMBER_DAO);
 
         initTable();
+        initAddForm();
+        initDeleteForm();
+        initUpdateForm();
 
-        setLayout(new BorderLayout());
-        add(scrollPane, BorderLayout.CENTER);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(addFormPanel);
+        add(deleteFormPanel);
+        add(updateFormPanel);
+        add(scrollPane);
+    }
+
+    private void initAddForm() {
+        addFormPanel = new JPanel();
+
+        JTextField nameField = new JTextField(20);
+        JButton addButton = new JButton("add");
+
+        addFormPanel.add(new JLabel("[Create]"));
+        addFormPanel.add(new JLabel("name: "));
+        addFormPanel.add(nameField);
+        addFormPanel.add(addButton);
+    }
+
+    private void initDeleteForm() {
+        deleteFormPanel = new JPanel();
+
+        JTextField idField = new JTextField(5);
+        JButton deleteButton = new JButton("delete");
+
+        deleteFormPanel.add(new JLabel("[Delete]"));
+        deleteFormPanel.add(new JLabel("id: "));
+        deleteFormPanel.add(idField);
+        deleteFormPanel.add(deleteButton);
+    }
+
+    private void initUpdateForm() {
+        updateFormPanel = new JPanel();
+
+        JTextField idField = new JTextField(5);
+        JTextField nameField = new JTextField(20);
+        JButton updateButton = new JButton("Update");
+
+        updateFormPanel.add(new JLabel("[Update]"));
+        updateFormPanel.add(new JLabel("id: "));
+        updateFormPanel.add(idField);
+        updateFormPanel.add(new JLabel("name: "));
+        updateFormPanel.add(nameField);
+        updateFormPanel.add(updateButton);
     }
 
     private void initTable() throws SQLException {
-        DefaultTableModel dtm = new DefaultTableModel(0, 0);
+        DefaultTableModel dtm = new DefaultTableModel(0, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         dtm.setColumnIdentifiers(columnNames);
 
         for(Member m: dao.getAll()) {
